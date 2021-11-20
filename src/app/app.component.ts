e2a59345c7708ceb4a2341fb3ef6fa847a18bf8b
suphawit19 <suphawit1942@gmail.com>
2021-11-20 12:32:09 +0700

    html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface  PeriodicElement{
   txn_date: String;
@@ -20,5 +21,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class AppComponent {
   title = 'covid19';
   displayedColumns: string[] = ['province', 'new_case','total_case', 'new_case_excludeabroad', 'total_case_excludeabroad', 'new_death', 'total_death', 'update_date'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
